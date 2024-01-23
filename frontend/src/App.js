e95eb44bd842f8dbe2034011dc2a650c97e2e302
zhac299 <zhac299@live.rhul.ac.uk>
2024-01-23 17:04:08 +0000

    React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/UserInput';
+import UserInput from './components/UserInput';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/UserInput.js
@@ -0,0 +1,14 @@
+function UserInput() {
+    return (
+        <>
+        <form>
+            <label>
+                Enter your vehicle requirements:
+                <input type="text" />
+            </label>
+        </form>
+        </>
+    );
+}
+
+export default UserInput;
